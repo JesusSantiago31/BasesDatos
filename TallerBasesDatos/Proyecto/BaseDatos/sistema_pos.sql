7c5aabf7afa505adbd033f6d79b1d558fe8e5dbe
JesusSantiago31 <jesussantiagocr31@gmail.com>
2025-01-01 03:07:00 -0600

    Tabla Clientes

--- TallerBasesDatos/Proyecto/BaseDatos/sistema_pos.sql
+++ TallerBasesDatos/Proyecto/BaseDatos/sistema_pos.sql
@@ -1,51 +1,17 @@
 -- 1.- Creacion y uso de la base de datos
 CREATE DATABASE IF NOT EXISTS sistema_pos;
+-- 2.- usar l base de datos creada 
 USE sistema_pos;
 
--- TABLAS PRINCIPALES
 -- Tabla de Clientes
 CREATE TABLE clientes (
-    id_cliente INT AUTO_INCREMENT PRIMARY KEY, 
-    nombre VARCHAR(100) NOT NULL, 
-    correo VARCHAR(100) UNIQUE, 
-    fecha_registro DATE 
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,  -- Identificador uico
+    nombre VARCHAR(100) NOT NULL,               -- Nombre del cliente 
+    correo VARCHAR(100) UNIQUE,                 -- Correo Unico para cada cliente
+    fecha_registro DATE NOT NULL,                        -- Fecha de registro del cliente 
+    numero_compras INT DEFAULT 0 NOT NULL,      -- Numero de compras inicializada en 0
+    
+    CONSTRAINT chk_correo CHECK (correo LIKE '%_@__%.__%'), -- Constrain para verificar el formato de e-mail
+    CONSTRAINT chk_nombre CHECK (CHAR_LENGTH(nombre) >= 3)  -- Constrain para la longitud del nombre
+ 
 );
-
--- Tabla de Productos
-CREATE TABLE productos (
-    id_producto INT AUTO_INCREMENT PRIMARY KEY, 
-    nombre_producto VARCHAR(100) NOT NULL,
-    precio DECIMAL(10,2) NOT NULL, 
-    stock INT DEFAULT 0 
-);
-
--- Tabla de Ventas
-CREATE TABLE ventas (
-    id_venta INT AUTO_INCREMENT PRIMARY KEY,
-    id_cliente INT,
-    fecha_venta DATE, 
-    total DECIMAL(10,2), 
-    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
-);
-
--- Tabla de Tarjeta de Puntos
-CREATE TABLE tarjeta_puntos (
-    id_tarjeta INT AUTO_INCREMENT PRIMARY KEY,
-    id_cliente INT,
-    puntos_acumulados INT DEFAULT 0, 
-    fecha_ultima_actualizacion DATE,
-    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
-);
-
--- Tabla de Historial de Cambios
-CREATE TABLE historial_cambios (
-    id_cambio INT AUTO_INCREMENT PRIMARY KEY, 
-    id_cliente INT, 
-    columna_modificada VARCHAR(50), 
-    valor_anterior VARCHAR(100), 
-    valor_nuevo VARCHAR(100),
-    fecha_cambio DATE,
-    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
-);
-
-drop table historial_cambios;
