d6c74c9cd06dfe103e68f5ab24babe0607c49f3a
JesusSantiago31 <jesussantiagocr31@gmail.com>
2025-01-06 03:07:00 -0600

    Funcion para almacenar fecha registro

--- TallerBasesDatos/Proyecto/BaseDatos/Funciones.sql
+++ TallerBasesDatos/Proyecto/BaseDatos/Funciones.sql
@@ -15,3 +15,15 @@ BEGIN
     RETURN stock; -- Devolver la cantidad de stock disponible
 END //
 DELIMITER ;
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `ObtenerFechaRegistro` (`p_id_cliente` INT) RETURNS DATE  BEGIN
+    DECLARE fecha_registro DATE; -- Variable para almacenar la fecha de registro
+
+    -- Obtener la fecha de registro del cliente especificado
+    SELECT fecha_registro INTO fecha_registro
+    FROM clientes
+    WHERE id_cliente = p_id_cliente; -- Filtrar por ID del cliente
+
+    RETURN fecha_registro; -- Devolver la fecha de registro del cliente
+END$$
+
